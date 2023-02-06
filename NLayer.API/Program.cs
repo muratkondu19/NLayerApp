388d55ed3ce792870f42eab3a09f5d84d25a35be
Murat Kondu <murat.kondu.mk@hotmail.com>
2023-02-06 21:48:47 +0300

    Repository- Migrations işlemlerinin yapılması

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Repositories;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Repository;
+using NLayer.Repository.Repositories;
+using NLayer.Repository.UnitOfWork;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//appsetting.jsonda yer alan connection string için
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    //Configuration.GetConnectionString("SqlConnection") -> appsettingste olan SqlConnection ý alýr.
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"),
+        options =>
+        {
+            /*
+             * Migration dosyalarý ve repositoryde oluþacak ve app db context’de repository katmanýnda olduðundan appdbcontextin bulunduðu assembly API Katmanýnda uygulamaya haberdar edilmelidir. bunun için options parametresi geçilir.
+             * Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name-> bize NLayer.Repository assembly adýný verir
+             */
+            options.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+        });
+});
+
+//Migration yapýlmasý için repo ve implementasyonlarýn eklenmesi
+//generic olanlar typeopf ile kullanýlýr
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+//sadece bir adet T entitiy generiz aldýðý için <> yeterli fakat çoklu olsaydý <,> her biri için , eklenecekti
+builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericeRepository<>));
+//builder.Services.AddScoped(typeof(IService<>),typeof(Service<>));
 
 var app = builder.Build();
 
--- NLayer.Repository/Seeds/ProductSeed.cs
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -20,7 +20,7 @@ namespace NLayer.Repository.Seeds
                 new Product { Id = 2, Name = "Kalem 2", CategoryId = 1, Price = 200, Stock = 20, CreatedDate = DateTime.Now },
                 new Product { Id = 3, Name = "Kalem 3", CategoryId = 1, Price = 300, Stock = 20, CreatedDate = DateTime.Now },
                 new Product { Id = 4, Name = "Kitap 1", CategoryId = 2, Price = 150, Stock = 50, CreatedDate = DateTime.Now },
-                new Product { Id = 4, Name = "Kitap 2", CategoryId = 2, Price = 160, Stock = 70, CreatedDate = DateTime.Now });
+                new Product { Id = 5, Name = "Kitap 2", CategoryId = 2, Price = 160, Stock = 70, CreatedDate = DateTime.Now });
         }
     }
 }
